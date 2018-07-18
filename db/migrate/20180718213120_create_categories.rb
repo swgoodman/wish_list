a923ac3667af6e33f9564b7aa5172f7c6c54df64
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-18 15:24:19 -0700

    updated table names

--- db/migrate/20180718213120_create_categories.rb
+++ db/migrate/20180718213120_create_categories.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration
   def change
-    create_table :category do |t|
+    create_table :categories do |t|
       t.string :name
 
       t.timestamps null: false
