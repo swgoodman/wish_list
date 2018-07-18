6cb966d687060c5f98ce800d6ed783a723b05573
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-18 14:35:13 -0700

    create user table

--- db/migrate/20180718011106_create_users.rb
+++ db/migrate/20180718011106_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    creat_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      
+      t.timestamps null: false
   end
 end
