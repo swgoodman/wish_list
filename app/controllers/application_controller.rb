7d1cb07ef5c9d16b8963b0fbd02b7f82d4caeb32
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-18 15:32:22 -0700

    created index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    
+
     enable :sessions
     set :session_secret, "talk_the_talk"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
 end
