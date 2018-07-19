5542daa813373f7649769ed7cda3b3ac3bbd0f99
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-19 10:36:40 -0700

    add item functionality build

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,4 +6,13 @@ class ItemsController < ApplicationController
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/:slug/list/add' do
+    @user = User.find_by_slug(params[:slug])
+    if logged_in?
+      erb :'item/create_item'
+    else
+      redirect to '/login'
+    end
+  end
 end
