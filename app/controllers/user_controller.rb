34278d129ddb3299f480f135a84f16a84c0d417a
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-19 09:52:43 -0700

    simplified user route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
 
-      redirect to "/users/#{@user.name}/list"
+      redirect to "/#{@user.name}/list"
     else
       erb :'user/login'
     end
@@ -30,11 +30,11 @@ class UsersController < ApplicationController
       @user.save
       session[:user_id] = @user.id
 
-      redirect to '/users/@user.slug/list'
+      redirect to "/#{@user.name}/list"
     end
   end
 
-  get '/users/:slug/list' do
+  get '/:slug/list' do
     @user = User.find_by_slug(params[:slug])
     erb :'user/show_list'
   end
