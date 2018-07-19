2f80345f80c543b015e75c3b0a1a5111b922b347
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-19 10:52:03 -0700

    user specific routes

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -8,11 +8,29 @@ class ItemsController < ApplicationController
   end
 
   get '/:slug/list/add' do
-    @user = User.find_by_slug(params[:slug])
     if logged_in?
+      @user = User.find_by_slug(params[:slug])
       erb :'item/create_item'
     else
       redirect to '/login'
     end
   end
+
+  post '/:slug/list/add' do
+  @user = User.find_by_slug(params[:slug])
+  if logged_in?
+    if params[:name] == "" || params[:link] == "" || params[:price] == "" || params[:category] == ""
+      redirect to "/#{@user.slug}/list/add"
+    else
+      @tweet = current_user.tweets.build(content: params[:content])
+      if @tweet.save
+        redirect to "/tweets/#{@tweet.id}"
+      else
+        redirect to "/tweets/new"
+      end
+    end
+  else
+    redirect to '/login'
+  end
+end
 end
