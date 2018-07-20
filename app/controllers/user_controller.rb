67111022e5ed5df72ad9a1617692e3964fe4331c
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-20 12:33:44 -0700

    project functional

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -44,4 +44,13 @@ class UsersController < ApplicationController
       end
     end
 
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
