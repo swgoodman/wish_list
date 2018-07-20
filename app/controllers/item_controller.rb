11d445f68b1f10fec89c7a11df3ecbcb7741d6cd
“swgoodman” <spencerwgoodman@icloud.com>
2018-07-20 12:43:35 -0700

    clean up list view

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -19,16 +19,16 @@ class ItemsController < ApplicationController
   post '/:slug/list/add' do
   if logged_in?
 
-    if params[:name] == "" || params[:link] == "" || params[:price] == ""# || params[:item[:category_id] == ""
+    if params[:name] == "" || params[:link] == "" || params[:price] == ""
       redirect to "/#{current_user.slug}/list/add"
     else
       @item = current_user.item.build(name: params[:name], link: params[:link], price: params[:price])
 
-      if params[:category][:name] == ""
-        redirect to "/#{current_user.slug}/list/add"
-      else
-        @item.category_id = Category.find_or_create_by(name: params[:category][:name])
-      end
+      # if params[:category][:name] == ""
+      #   redirect to "/#{current_user.slug}/list/add"
+      # else
+      #   @item.category_id = Category.find_or_create_by(name: params[:category][:name])
+      # end
 
       if @item.save
         redirect to "/#{current_user.slug}/list"
