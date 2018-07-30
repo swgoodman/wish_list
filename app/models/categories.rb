013046e7dbc7752a322533538fe3eded295af996
swgoodman <spencerwgoodman@icloud.com>
2018-07-30 09:59:29 -0700

    added comments to views

--- app/models/categories.rb
+++ app/models/categories.rb
@@ -2,8 +2,10 @@ require_relative "concerns/slugifiable.rb"
 
 class Category < ActiveRecord::Base
 
+# Gives class 'Slug' functionality.
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
 
+# Class Associations.
   has_many :items
 end
